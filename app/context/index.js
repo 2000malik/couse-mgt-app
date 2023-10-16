7953046c4e34fc4591171f4e9fb77e428c6ad8c0
Malik <36223770+2000malik@users.noreply.github.com>
2023-10-16 17:55:50 +0100

    user profile feat added

--- app/context/index.js
+++ app/context/index.js
@@ -6,9 +6,16 @@ export const AppContext = createContext({});
 export default function AppContainer({ children }) {
   const [Course, setCourse] = useState([]);
   const [instructor, setInstructor] = useState([]);
+  const [userProfile, setUserProfile] = useState({
+    name: 'Malik ojo',
+    userId: Date.now(),
+    email: 'malikkorede99@gmail.com',
+    phoneNumber: '+2348141258672',
+  });
 
   return (
-    <AppContext.Provider value={{ Course, setCourse, instructor, setInstructor }}>
+    <AppContext.Provider
+      value={{ Course, setCourse, instructor, setInstructor, userProfile, setUserProfile }}>
       {children}
     </AppContext.Provider>
   );
